b10359b6b18a7b2c5e44949ed129daa0fe5bb3c0
Castor Troy <castor@troynote>
2015-05-24 22:20:34 +1200

    testing if date is right

--- src/nz/co/c4stortroy/initwithobjects/Date.java
+++ src/nz/co/c4stortroy/initwithobjects/Date.java
@@ -10,9 +10,32 @@ public class Date {
 	this.day = day;
 	this.month = month;
 	this.year = year;
+	if (! isDateAvailable(day, month, year)) {
+            System.out.println("A data " + formatDate() + " não existe!");
+        }
     }
     public String formatDate(){
 	String date = this.day + "/"+ this.month + "/"+ + this.year;
 	return date;
+    }
+    
+    private boolean isDateAvailable(int day, int month, int year) {
+        if (day <= 0 || month <= 0) {
+            return false;
+        }
+        int lastDayMonth = 31; // por padrao são 31 dias
+        if (month == 4 || month == 6 || month == 9 || month == 11 ) {
+            lastDayMonth = 30;
+        } else if (month == 2) {
+            if (year % 4 == 0) {
+        	lastDayMonth = 29;
+            } else {
+        	lastDayMonth = 28;
+            }
+        }
+        if (day > lastDayMonth) {
+            return false;
+        }
+        return true;
     }
 }
